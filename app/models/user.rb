42e1d254405bd15d6131d36f096c916528916b09
arturaraujo_b <arturaraujo.b@gmail.com>
2016-11-02 16:16:08 +0000

    Making user e-mail downcase

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ActiveRecord::Base
     
+    before_save { self.email = self.email.downcase }
+    
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
       validates :email, presence: true, length: { maximum: 255 },
                         format: { with: VALID_EMAIL_REGEX },
                         uniqueness: { case_sensitive: false }
+                        
 
 end
